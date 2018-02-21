f043c6a02ba67706ecfd41e566d21e493aee324b
anup <anup1s@outlook.com>
2018-02-21 15:01:34 +0530

    dsl-files-jenkins: file evalution in groovy - intermediate 3
    Change-Id: I6c755a33fbf39db29f0e64e9c1b1848a6e5a8447

--- vipre-nis/assembly.groovy
+++ vipre-nis/assembly.groovy
@@ -6,8 +6,13 @@ def vipreNisDir = WORKSPACE + File.separator + "vipre-nis"
 
 println(vipreNisDir)
 
-evaluate(new File(vipreNisDir + File.separator + "job1.groovy"))
-evaluate(new File(vipreNisDir + File.separator + "job2.groovy"))
+//evaluate(new File(vipreNisDir + File.separator + "job1.groovy"))
+//evaluate(new File(vipreNisDir + File.separator + "job2.groovy"))
+
+import utilities.MyUtilities
+
+def myJob = job('example')
+MyUtilities.addMyFeature(myJob)
 
 listView('githubView') {
     jobs {
