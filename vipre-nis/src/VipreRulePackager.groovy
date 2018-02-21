04bc38614155e49344a2bbb87aa0d9684fe31a07
anup <anup1s@outlook.com>
2018-02-21 15:55:44 +0530

    dsl-files-jenkins: file evalution in groovy - intermediate 3
    Change-Id: Ib5b9dc72ff02475218a936a45a43c5933931a76d

--- vipre-nis/src/VipreRulePackager.groovy
+++ vipre-nis/src/VipreRulePackager.groovy
@@ -1,17 +1,18 @@
 class VipreRulePackager {
-
     static void generate(def job) {
-        wrappers {
-            colorizeOutput()
-            timestamps()	
-        }
+        job.with {
+            wrappers {
+                colorizeOutput()
+                timestamps()	
+            }
 
-    publishers {
-            extendedEmail {
-                recipientList('omigd33@gmail.com')
-                defaultSubject('$DEFAULT_SUBJECT')
-                defaultContent('$DEFAULT_CONTENT')
-                attachBuildLog(true)
+            publishers {
+                extendedEmail {
+                    recipientList('omigd33@gmail.com')
+                    defaultSubject('$DEFAULT_SUBJECT')
+                    defaultContent('$DEFAULT_CONTENT')
+                    attachBuildLog(true)
+                }
             }
         }
     }
