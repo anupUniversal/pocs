88b1f4f9db73642385ba6cbec0723d7e8667be25
anup <anup1s@outlook.com>
2018-02-22 14:22:08 +0530

    dsl-files-jenkins: vipre-site-connectors jira OP-1608
    Change-Id: I2a51f0927d03d98e1649e1f8c92fe5443bc87b1f

---
+++ vipre-site-connectors/pipeline.groovy
@@ -0,0 +1,60 @@
+
+job('vipre-site-connectors') {
+
+    label('vs2017&&wix')
+	description('')
+	
+	wrappers {
+		colorizeOutput()
+        timestamps()
+        deliveryPipelineVersion('1.0.${BUILD_NUMBER}', true)
+    }
+	
+	multiscm {
+        git {
+            remote {
+				url('https://bitbucket.org/threattrack/vipre-site-connectors.git')
+            }
+			branch('*/master')
+        }
+    }
+
+    steps {
+        msBuild {
+            msBuildInstallation('MSBuild 1.28')
+            buildFile('source\VipreConnectors.sln')
+            args()
+            passBuildVariables()
+            continueOnBuildFailure()
+            unstableIfWarnings()
+        }
+    }
+	
+   publishers {
+        extendedEmail {
+            recipientList('Zachary.Scolaro@vipre.com')
+            recipientList('Nelson.Toro@jalasoft.com')
+            recipientList('Marcel.Morales@jalasoft.com')
+            defaultSubject('$DEFAULT_SUBJECT')
+            defaultContent('$DEFAULT_CONTENT')
+			attachBuildLog(true)
+        }
+    }
+}
+
+listView('vipre-site-connectors-view') {
+    jobs {
+      name('vipre-site-connectors')
+      name('vipre-site-connectors-seed')
+    }
+  
+   columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
